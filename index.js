0ea9a9260b2466ae9802157a8ea26d5ac5ed60ae
knowicki <konrad.nowicki@capgemini.com>
2022-09-13 15:22:58 +0200

    added some fixes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/productsdb", {
+mongoose.connect('mongodb://localhost/productsdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -16,6 +16,6 @@ mongoose.connect("mongodb://localhost/productsdb", {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send(`Store server running on port ${PORT}`));
+app.get('/', (req, res) => res.send(`Store server running on port ${PORT}`));
 
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
